6be3e7e111f3d3ebba7d904c6d43207f668abca6
LiXinShuo <lxshelloworld@gmail.com>
2025-06-05 17:12:39 +0900

    gitignoreについての設定

---
+++ note-backend/src/test/java/com/lxs/note_backend/NoteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lxs.note_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
