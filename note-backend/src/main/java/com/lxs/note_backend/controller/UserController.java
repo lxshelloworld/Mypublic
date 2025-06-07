3921af1131d168750d7ac85fba3f75aea638e8d6
LiXinShuo <lxshelloworld@gmail.com>
2025-06-07 20:21:28 +0900

    ユーザー新規登録機能

---
+++ note-backend/src/main/java/com/lxs/note_backend/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.lxs.note_backend.controller;
+
+
+import com.lxs.note_backend.entity.User;
+import com.lxs.note_backend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public String register(@RequestBody User user) {
+        return userService.register(user);
+    }
+}
